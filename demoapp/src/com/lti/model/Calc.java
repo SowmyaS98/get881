67951824b81b0b7ec5542767dd7f0d6d0aa58eb5
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2020-12-18 15:49:10 +0530

    latest commit

--- demoapp/src/com/lti/model/Calc.java
+++ demoapp/src/com/lti/model/Calc.java
@@ -1,14 +1,7 @@
 package com.lti.model;
 
 public class Calc {
-	public void add(int n1,int n2)
-	{
-		System.out.println("sum is: "+n1+n2);
-	}
-	public void mul(int n1,int n2)
-	{
-		System.out.println("mul is: "+(n1*n2));
-	}
+	
 	public void div(int n1,int n2)
 	{
 		System.out.println("div is: "+(n1/n2));
